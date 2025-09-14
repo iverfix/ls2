aef6cd2e033c765d1951bb5dd19e9e3c025c23c1
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-09-14 15:58:48 +0200

    Updated directory structure

--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include "filehandler.h"
 #include <filesystem>
-#include <iostream>
 
 
    
@@ -11,7 +10,6 @@ std::vector<std::string> FileHandler::getFileList(){
     std::vector<std::string> paths{};
     for (auto const& entry : std::filesystem::directory_iterator{"."}){
         paths.push_back(entry.path());
-        std::cout << "Her da folkens" << std::endl;
     }
 
     return paths;
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ int main() {
     FileHandler fh{};
     std::vector<std::string> files = fh.getFileList();
     
-    std::cout << "Hei verden " << files.size() <<std::endl;
-
     for (const auto& file : files){
         std::cout << file << std::endl;
     }
