7085e55cd4f2c8d932abd1093c29d8c5d4ee6c57
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-09-16 11:28:03 +0200

    Updated all

--- src/argumentParser.cpp
+++ src/argumentParser.cpp
@@ -9,7 +9,7 @@ UserOptions parseArgs(int argc, const char* argv[]){
   UserOptions opts{};
  
   for (const std::string_view arg : std::span{argv + 1, static_cast<size_t>(argc -1)}) {
-    if (arg == "-a")
+    if (arg == "-a" || arg == "--all")
       opts.showHiddenFiles = true;
     else
       throw std::runtime_error("Invalid input parameter");
