6d01de3412790f25e450d9036742b440877aae51
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-09-23 18:37:22 +0200

    Fixed thread safty

--- src/unixOperatingSystem.cpp
+++ src/unixOperatingSystem.cpp
@@ -4,31 +4,54 @@
 #include <stdexcept>
 #include <string>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
+
+
+constexpr int DEFAULT_BUFFER_SIZE = 16384;
 
 std::string UnixOperatingSystem::getFileUser(const char* filename) const
 {
-
   struct stat fileStat
   {
   };
   if (stat(filename, &fileStat) == -1) { throw std::runtime_error("File could not be found"); }
 
+  struct passwd pwd
+  {
+  };
+  struct passwd* result = nullptr;
+  long bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+  if (bufsize == -1) {
+    bufsize = DEFAULT_BUFFER_SIZE;// Fallback value
+  }
 
-  struct passwd* fileUser = getpwuid(fileStat.st_uid);
+  std::vector<char> buffer(bufsize);
 
-  return { fileUser != nullptr ? fileUser->pw_name : "unknown" };
+  if (getpwuid_r(fileStat.st_uid, &pwd, buffer.data(), buffer.size(), &result) != 0 || result == nullptr) { return "unknown"; }
+
+  return { pwd.pw_name };
 }
 
 std::string UnixOperatingSystem::getFileGroup(const char* filename) const
 {
-
-
   struct stat fileStat
   {
   };
   if (stat(filename, &fileStat) == -1) { throw std::runtime_error("File could not be found"); }
 
-  struct group* groupStats = getgrgid(fileStat.st_gid);
+  struct group grp
+  {
+  };
+  struct group* result = nullptr;
+  long bufsize = sysconf(_SC_GETGR_R_SIZE_MAX);
+  if (bufsize == -1) {
+    bufsize = DEFAULT_BUFFER_SIZE;// Fallback value
+  }
 
-  return { groupStats != nullptr ? groupStats->gr_name : "unknown" };
+  std::vector<char> buffer(bufsize);
+
+  if (getgrgid_r(fileStat.st_gid, &grp, buffer.data(), buffer.size(), &result) != 0 || result == nullptr) { return "unknown"; }
+
+  return { grp.gr_name };
 }
