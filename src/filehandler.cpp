032f0a16fe6b88b346da471c256001de45ec8514
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-10-02 17:19:08 +0200

    Updated filehandler loop

--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -9,20 +9,20 @@
 std::vector<Entry> FileHandler::getFolderContent() const
 {
 
-  auto filter = [&](const std::filesystem::directory_entry& entry) { return options.showHiddenFiles || !entry.path().filename().string().starts_with("."); };
+  auto filter = [&](const std::filesystem::directory_entry& entry) { return options.showHiddenFiles || !entry.path().filename().native().starts_with("."); };
   auto transform = [&](const std::filesystem::directory_entry& entry) -> Entry {
     std::string filename = entry.path().filename().string();
     const uintmax_t fileSize = entry.is_directory() ? 4096 : entry.file_size();
-    return { .entryName = filename,
-      .entryGroup = fileSystem.getFileGroup(filename.data()),
-      .userGroup = fileSystem.getFileUser(filename.data()),
+    return { .entryName = std::move(filename),
+      .entryGroup = fileSystem.getFileGroup(filename.c_str()),
+      .userGroup = fileSystem.getFileUser(filename.c_str()),
       .bytesize = fileSize,
       .lastWriteTime = 0,
       .type = getFileType(entry)
 
     };
   };
-  auto entries = std::filesystem::directory_iterator{ ".", std::filesystem::directory_options::skip_permission_denied } | std::ranges::views::filter(filter)
+  auto entries = std::filesystem::directory_iterator{ ".", std::filesystem::directory_options::skip_permission_denied } | std::views::filter(filter)
                  | std::views::transform(transform) | std::ranges::to<std::vector>();
 
   std::ranges::sort(entries, {}, &Entry::entryName);
