b3ca4f39321030a31b5f8eb22e220d1ca0aa2dfe
Iver <iver.andreas.ugelvik@hotmail.com>
2025-10-12 16:42:38 +0200

    Fixed linting issue

--- src/unixOperatingSystem.cpp
+++ src/unixOperatingSystem.cpp
@@ -11,11 +11,15 @@
 constexpr int DEFAULT_BUFFER_SIZE = 16384;
 
 long UnixOperatingSystem::getNumHardLinks(const char* filename) const {
-  struct stat fileStat{};
+  struct stat fileStat
+  {
+  };
 
   if (stat(filename, &fileStat) == -1) throw std::runtime_error("File coluld not be found");
 
-  struct passwd pwd{};
+  struct passwd pwd
+  {
+  };
   struct passwd* result{ nullptr };
   long bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
 
@@ -29,10 +33,14 @@ long UnixOperatingSystem::getNumHardLinks(const char* filename) const {
 }
 
 std::string UnixOperatingSystem::getFileUser(const char* filename) const {
-  struct stat fileStat{};
+  struct stat fileStat
+  {
+  };
   if (stat(filename, &fileStat) == -1) { throw std::runtime_error("File could not be found"); }
 
-  struct passwd pwd{};
+  struct passwd pwd
+  {
+  };
   struct passwd* result = nullptr;
   long bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
   if (bufsize == -1) {
@@ -47,10 +55,14 @@ std::string UnixOperatingSystem::getFileUser(const char* filename) const {
 }
 
 std::string UnixOperatingSystem::getFileGroup(const char* filename) const {
-  struct stat fileStat{};
+  struct stat fileStat
+  {
+  };
   if (stat(filename, &fileStat) == -1) { throw std::runtime_error("File could not be found"); }
 
-  struct group grp{};
+  struct group grp
+  {
+  };
   struct group* result = nullptr;
   long bufsize = sysconf(_SC_GETGR_R_SIZE_MAX);
   if (bufsize == -1) {
