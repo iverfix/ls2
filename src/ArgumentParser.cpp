3d3de524e4fa23a7b5ff06e5c60a2db8bcadc272
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-10-29 14:44:22 +0100

    Added new parsing

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -2,6 +2,36 @@
 #include <span>
 #include <stdexcept>
 #include <string_view>
+#include <map>
+#include <vector>
+#include <ranges>
+
+
+std::vector<std::string> extractFeatureFlags(std::span<const char*> args) {
+
+  // The map is necessary to perserve ordering later, regardless of user input
+  std::map<std::string, bool> enabledFeatureFlags{
+    {"a", false},
+    {"g", false},
+    {"l", false},
+    {"o", false},
+    {"G", false}
+  };
+
+  for (const std::string_view arg : args.subspan(1)) {
+    if (arg.starts_with("--")) {
+
+    } else {
+      for (const char flag : arg.substr(1)) {
+        enabledFeatureFlags[std::string(1, flag)] = true;
+      }
+    }
+
+  } 
+
+  auto view = enabledFeatureFlags | std::views::filter([](const auto& pair) { return pair.second; }) | std::views::transform([](const auto& pair) { return pair.first; });
+  return {view.begin(), view.end()};
+}
 
 UserOptions parseArgs(std::span<const char*> args)
 {
@@ -10,11 +40,19 @@ UserOptions parseArgs(std::span<const char*> args)
 
   UserOptions opts{};
 
+  /*
+  * 1. Create a vector of pairs with true false flags set
+  * 2. Extract all letters or flags from the input
+  * 3. Filter through the entire object for true values
+  * 4. Build UserOptions from the filtered vector of pairs
+  * 5. Switch-case on selection to make it instant and perserve evaluation ordering
+  */
 
-  for (const std::string_view arg : args.subspan(1)) {
-    if (arg == "-a" || arg == "--all") {
+
+  for (const auto& arg : extractFeatureFlags(args)) {
+    if (arg == "a" || arg == "--all") {
       opts.showHiddenFiles = true;
-    } else if (arg == "-l") {
+    } else if (arg == "l") {
       opts.showLongFormat = true;
       opts.longListOptions.showFilename = true;
       opts.longListOptions.showOwnerGroup = true;
@@ -23,7 +61,7 @@ UserOptions parseArgs(std::span<const char*> args)
       opts.longListOptions.showBytesize = true;
       opts.longListOptions.showWriteTime = true;
       opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-g") {
+    } else if (arg == "g") {
       opts.showLongFormat = true;
       opts.longListOptions.showFilename = true;
       opts.longListOptions.showOwnerGroup = true;
@@ -32,7 +70,7 @@ UserOptions parseArgs(std::span<const char*> args)
       opts.longListOptions.showBytesize = true;
       opts.longListOptions.showWriteTime = true;
       opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-o") {
+    } else if (arg == "o") {
       opts.showLongFormat = true;
       opts.longListOptions.showFilename = true;
       opts.longListOptions.showOwnerGroup = false;
@@ -41,7 +79,7 @@ UserOptions parseArgs(std::span<const char*> args)
       opts.longListOptions.showBytesize = true;
       opts.longListOptions.showWriteTime = true;
       opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-G" || arg == "--no-group") {
+    } else if (arg == "G" || arg == "--no-group") {
       opts.longListOptions.showOwnerGroup = false;
     } else {
       throw std::invalid_argument("Invalid input parameter");
