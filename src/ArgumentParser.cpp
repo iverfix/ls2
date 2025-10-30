f28197f45cd981560bcbc02c492792ead14c5b05
iverfix <36566491+iverfix@users.noreply.github.com>
2025-10-30 14:04:36 +0100

    Feature/#50 multi arg parsing (#52)
    * Added new parsing
    
    * Fixed large args
    
    * Modernized loop
    
    * more modernization
    
    * Performed linting
    
    * Correct formatting
    
    * More scalable argument parsing
    
    * Added formatting
    
    * Update include/ArgumentParser.h
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update include/Display.h
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -1,7 +1,73 @@
 #include "ArgumentParser.h"
+#include <array>
+#include <functional>
+#include <ranges>
 #include <span>
 #include <stdexcept>
-#include <string_view>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+void flag_a(UserOptions& options) { options.showHiddenFiles = true; }
+
+void flag_l(UserOptions& options)
+{
+  options.showLongFormat = true;
+  options.longListOptions = LongListFormatOptions::enableAllLongListOptions();
+}
+
+void flag_g(UserOptions& options)
+{
+  options.showLongFormat = true;
+  options.longListOptions = LongListFormatOptions::enableAllLongListOptions();
+  options.longListOptions.showUserGroup = false;
+}
+
+void flag_o(UserOptions& options)
+{
+  options.showLongFormat = true;
+  options.longListOptions = LongListFormatOptions::enableAllLongListOptions();
+  options.longListOptions.showOwnerGroup = false;
+}
+
+void flag_G(UserOptions& options) { options.longListOptions.showOwnerGroup = false; }
+
+
+std::unordered_map<std::string, std::function<void(UserOptions&)>> getFlagActions()
+{
+
+  const static std::unordered_map<std::string, std::function<void(UserOptions&)>> flagActions{
+    { "a", flag_a }, { "all", flag_a }, { "l", flag_l }, { "g", flag_g }, { "o", flag_o }, { "G", flag_G }, { "no-group", flag_G }
+  };
+  return flagActions;
+}
+
+
+std::vector<std::string> extractFeatureFlags(std::span<const char*> args)
+{
+
+  // The map is necessary to perserve ordering later, regardless of user input
+  static constexpr std::array<std::string, 7> flagOrder{ "l", "a", "all", "g", "o", "G", "no-group" };
+  const std::unordered_set<std::string> allowedFlags{ flagOrder.begin(), flagOrder.end() };
+
+  std::unordered_set<std::string> enabledFlags{};
+
+  for (const std::string& arg : args.subspan(1)) {
+    if (arg.starts_with("--")) {
+      if (!allowedFlags.contains(arg.substr(2))) { throw std::invalid_argument("Invalid flag: " + arg); }
+      enabledFlags.emplace(arg.substr(2));
+    } else {
+      for (const char flag : arg.substr(1)) {
+        if (!allowedFlags.contains(std::string(1, flag))) { throw std::invalid_argument("Invalid flag: " + std::string{ flag }); }
+        enabledFlags.emplace(1, flag);
+      }
+    }
+  }
+
+  return flagOrder | std::ranges::views::filter([&enabledFlags](const std::string& flag) { return enabledFlags.contains(flag); }) | std::ranges::to<std::vector<std::string>>();
+}
 
 UserOptions parseArgs(std::span<const char*> args)
 {
@@ -9,43 +75,8 @@ UserOptions parseArgs(std::span<const char*> args)
   if (args.empty()) { throw std::invalid_argument("At least one argument is expected"); }
 
   UserOptions opts{};
+  auto flagActions = getFlagActions();
 
-
-  for (const std::string_view arg : args.subspan(1)) {
-    if (arg == "-a" || arg == "--all") {
-      opts.showHiddenFiles = true;
-    } else if (arg == "-l") {
-      opts.showLongFormat = true;
-      opts.longListOptions.showFilename = true;
-      opts.longListOptions.showOwnerGroup = true;
-      opts.longListOptions.showUserGroup = true;
-      opts.longListOptions.showPermissionString = true;
-      opts.longListOptions.showBytesize = true;
-      opts.longListOptions.showWriteTime = true;
-      opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-g") {
-      opts.showLongFormat = true;
-      opts.longListOptions.showFilename = true;
-      opts.longListOptions.showOwnerGroup = true;
-      opts.longListOptions.showUserGroup = false;
-      opts.longListOptions.showPermissionString = true;
-      opts.longListOptions.showBytesize = true;
-      opts.longListOptions.showWriteTime = true;
-      opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-o") {
-      opts.showLongFormat = true;
-      opts.longListOptions.showFilename = true;
-      opts.longListOptions.showOwnerGroup = false;
-      opts.longListOptions.showUserGroup = true;
-      opts.longListOptions.showPermissionString = true;
-      opts.longListOptions.showBytesize = true;
-      opts.longListOptions.showWriteTime = true;
-      opts.longListOptions.showNumHardLinks = true;
-    } else if (arg == "-G" || arg == "--no-group") {
-      opts.longListOptions.showOwnerGroup = false;
-    } else {
-      throw std::invalid_argument("Invalid input parameter");
-    }
-  }
+  for (const auto& arg : extractFeatureFlags(args)) { flagActions[arg](opts); }
   return opts;
 }
