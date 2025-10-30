c2f079d5832d4d00a9b8027fa9ac147c2c57d01f
Iver Andreas Ugelvik <iver.andreas.ugelvik@hotmail.com>
2025-10-30 12:17:10 +0100

    more modernization

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -2,28 +2,26 @@
 #include <ranges>
 #include <span>
 #include <stdexcept>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 std::vector<std::string> extractFeatureFlags(std::span<const char*> args) {
 
-  std::vector<std::string> output{};
   // The map is necessary to perserve ordering later, regardless of user input
   static const std::vector<std::string> flagOrder{"l", "a", "all", "g", "o", "G", "no-group"};
-  std::unordered_map<std::string, bool> enabledFlags{};
+  std::unordered_set<std::string> enabledFlags{};
 
   for (const std::string& arg : args.subspan(1)) {
     if (arg.starts_with("--")) {
-      enabledFlags[arg.substr(2)] = true;
-
+      enabledFlags.emplace(arg.substr(2));
     } else {
       for (const char flag : arg.substr(1)) {
-        enabledFlags[std::string(1, flag)] = true;
+        enabledFlags.emplace(1, flag);
       }
     }
   } 
 
-  return flagOrder | std::ranges::views::filter([&enabledFlags](const std::string& flag) { return enabledFlags[flag];}) | std::ranges::to<std::vector<std::string>>();
+  return flagOrder | std::ranges::views::filter([&enabledFlags](const std::string& flag) { return enabledFlags.contains(flag);}) | std::ranges::to<std::vector<std::string>>();
 }
 
 UserOptions parseArgs(std::span<const char*> args)
@@ -32,15 +30,6 @@ UserOptions parseArgs(std::span<const char*> args)
   if (args.empty()) { throw std::invalid_argument("At least one argument is expected"); }
 
   UserOptions opts{};
-
-  /*
-  * 1. Create a vector of pairs with true false flags set
-  * 2. Extract all letters or flags from the input
-  * 3. Filter through the entire object for true values
-  * 4. Build UserOptions from the filtered vector of pairs
-  * 5. Switch-case on selection to make it instant and perserve evaluation ordering
-  */
-
 
   for (const auto& arg : extractFeatureFlags(args)) {
 
